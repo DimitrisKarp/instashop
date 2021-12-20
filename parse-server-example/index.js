55724c32080ef300d5376dd6d412e11b9152e007
JimKarp <jim.karpet@hotmail.com>
2021-12-20 23:45:09 +0200

    feat(angular): ng setup

--- parse-server-example/index.js
+++ parse-server-example/index.js
@@ -21,9 +21,6 @@ const config = {
   appId: process.env.APP_ID || '',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
-  },
 };
 
 const dashboard = new ParseDashboard({
